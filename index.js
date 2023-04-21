8da2c1adadfed20c22b025ae35d34220d9c9b8ef
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2023-04-20 21:46:25 -0300

    prueba uno

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const char1 = document.getElementById('car1').value 
-const char2 = document.getElementById('car2').value
+const paises = [
+    {
+        nombre: "Republica Argentina",
+        bandera:"banderas/barg.png",
+    },
+    {
+        nombre: "Brasil",
+        bandera:"banderas/bbra.png",
+    },
+    {
+        nombre: "Uruguay",
+        bandera:"banderas/buru.png",
+    },
+    {
+        nombre: "Paraguay",
+        bandera:"banderas/bpar.png",
+    },
+]
+
 const char3 = document.getElementById('car3')
 const char4 = document.getElementById('car4')
 const char5 = document.getElementById('car5')
-const char6 = document.getElementById('car6')
-const char7 = document.getElementById('car7')
+const imgBandera = document.getElementById('div2img')
+const pNombre = document.getElementById('div1p')
+
+const buttom = document.getElementById('btnSubmit')
 
 const descubrirPatente =()=>{
-
-    if(typeof char3 === 'number'){
+    if(!isNaN(char3.value)){
         return 'Argentina'
-    } else if (typeof char4 === 'string'){
+    } else if (isNaN(char4.value)){
         return 'Paraguay'
-    } else if (typeof char5 === 'string'){
+    } else if (isNaN(char5.value)){
         return 'Brasil'
-    } else if (typeof char5 === 'number'){
+    } else if (!isNaN(char5.value)){
         return 'Uruguay'
     }
 }
+const armarPatente=(pais)=>{
+    const paisFiltrado = paises.filter(paiSelect => paiSelect.nombre === pais)[0];
+    imgBandera.setAttribute('src', paisFiltrado.bandera)
+    pNombre.innerText = (paisFiltrado.nombre)
+    console.log(paisFiltrado)
+}
 
-console.log(descubrirPatente())
+buttom.addEventListener('click', function(event) {
+    event.preventDefault();
+    armarPatente(descubrirPatente())
+});
+
+
